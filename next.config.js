1c8bffa14b9c9f50a50f719557fb2b45414c9a30
Philena <philena@mit.edu>
2023-07-12 01:36:53 -0400

    Added node modules, changed config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     compiler: {
         // Enables the styled-components SWC transform
         styledComponents: true
