b8a3ec0c20480c84237a73f79a6511feed325e75
Philena <philena@mit.edu>
2023-07-12 01:38:54 -0400

    Changed config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    images: {
+      unoptimized: true,
+    },
     output: 'export',
     compiler: {
         // Enables the styled-components SWC transform
